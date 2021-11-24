8b60ed6a887f69a909d2a34794097044088ca586
tiagocv <tcverdade@hotmail.com>
2021-11-24 18:49:15 +0000

    Make extension open options page on extension click

--- scripts/background.js
+++ scripts/background.js
@@ -1,17 +1,3 @@
-let color = "#3aa757";
-
-setTimeout(() => {
-  if (navigator.mediaDevices) {
-    navigator.mediaDevices.getUserMedia({ audio: true }).catch(function () {
-      chrome.tabs.create({
-        url: chrome.extension.getURL("../html/options.html"),
-        selected: true,
-      });
-    });
-  }
-}, 100);
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log("Default background color set to %cgreen", `color: ${color}`);
+chrome.action.onClicked.addListener(function(tab){
+  chrome.runtime.openOptionsPage();
 });
