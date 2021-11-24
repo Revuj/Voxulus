a4034a4c2f0fc22f6cec9eb74e047fb229c46a3e
Revuj <revuj112@gmail.com>
2021-11-24 15:45:34 +0000

    listening commands from options page

--- scripts/options.js
+++ scripts/options.js
@@ -6,3 +6,39 @@ navigator.mediaDevices
   .catch(function (err) {
     console.log("No mic for you!");
   });
+
+var messages = ["Hey", "Hi, there!", "Hi!", "Hello"];
+
+if (annyang) {
+  annyang.debug();
+  console.log("We have annyang!");
+
+  var commands = {
+    Hello: hello,
+    "What is your name": myname,
+    "new tab": newtab,
+    "open tab": newtab,
+  };
+
+  function hello() {
+    var randomIndex = Math.round(Math.random() * messages.length);
+    console.log(
+      `%c ${messages[randomIndex]}`,
+      "color: green; font-weight:bold;"
+    );
+  }
+
+  function myname() {
+    console.log("My name is Billy!");
+  }
+
+  function newtab() {
+    window.open("", "_blank");
+  }
+
+  // Add Commands
+  annyang.addCommands(commands);
+
+  // Start listening
+  annyang.start();
+}
